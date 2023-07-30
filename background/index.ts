bdde4bc316d3ac19e606a76b898d7cf6371c00cd
canalun <i.am.kanaru.sato@gmail.com>
2023-07-30 22:23:40 +0900

    add fire timing

--- background/index.ts
+++ background/index.ts
@@ -7,17 +7,38 @@ chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error))
 
-chrome.tabs.onUpdated.addListener(async function () {
+function sendCurrentTabs() {
   const currentTabs: chrome.tabs.Tab[] = []
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab) => {
-      console.log(tab.title)
       currentTabs.push(tab)
     })
-
     chrome.runtime.sendMessage({
       name: 'updateTab',
       data: { currentTabs: currentTabs },
     })
   })
+  return
+}
+
+chrome.tabs.onCreated.addListener(async function () {
+  sendCurrentTabs()
+})
+chrome.tabs.onUpdated.addListener(async function () {
+  sendCurrentTabs()
+})
+chrome.tabs.onMoved.addListener(async function () {
+  sendCurrentTabs()
+})
+chrome.tabs.onRemoved.addListener(async function () {
+  sendCurrentTabs()
+})
+chrome.tabs.onReplaced.addListener(async function () {
+  sendCurrentTabs()
+})
+chrome.tabs.onAttached.addListener(async function () {
+  sendCurrentTabs()
+})
+chrome.tabs.onDetached.addListener(async function () {
+  sendCurrentTabs()
 })
